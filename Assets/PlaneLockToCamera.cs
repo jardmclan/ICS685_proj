181466292f4a23f55ca165e145e2cffa4494f1ce
unknown <mcleanj@hawaii.edu>
2019-10-28 16:40:06 -1000

    changing stategies, extruding water to collider

--- Assets/PlaneLockToCamera.cs
+++ Assets/PlaneLockToCamera.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneLockToCamera : MonoBehaviour
-{
+
+
+public class PlaneLockToCamera : MonoBehaviour {
+
+    public Camera camera;
+    public float clippingDistance = 1000;
+
+    //also need position
+    //static height (y axis)
+
+    //x pitch, y yaw, z roll
+    //x = 90def straight down
+    //-90 - 90deg have ground view at some distance
+    //get lowe
+    //forward +z, back -z
+    //left -x, right +x
+    //up +y, down -y
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        //for now, maximum
+        // camera.farClipPlane
+        // transform.localScale = new Vector3();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
---
+++ Assets/WaterModifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterModifier : MonoBehaviour {
+    private bool first = true;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(first) {
+            foreach(Transform child in transform) {
+                Debug.Log(child.name + "\n");
+                //"Water - 0"
+            }
+            first = false;
+        }
+        
+    }
+}
